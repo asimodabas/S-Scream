4b03d3f8f13bd45f73f8dcef0ce6d38b86b6f7ee
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-12-27 20:16:17 +0300

    feed and main act. desing added

---
+++ app/src/main/java/com/asimodabas/instagram_clone/UploadActivity.kt
@@ -0,0 +1,11 @@
+package com.asimodabas.instagram_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload)
+    }
+}
