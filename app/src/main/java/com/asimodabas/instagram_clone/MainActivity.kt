12c6027201b69063b5245e120d4155bf5eba0559
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-12-28 23:18:06 +0300

    binding and design added

--- app/src/main/java/com/asimodabas/instagram_clone/MainActivity.kt
+++ app/src/main/java/com/asimodabas/instagram_clone/MainActivity.kt
@@ -2,11 +2,33 @@ package com.asimodabas.instagram_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asimodabas.instagram_clone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.signInButton.setOnClickListener {
+           signInClicked()
+        }
+
+        binding.signUpButton.setOnClickListener {
+            signUpClicked()
+        }
+    }
+
+    fun signInClicked(){
+
+    }
+
+    fun signUpClicked(){
 
     }
 }
--- app/src/main/java/com/asimodabas/instagram_clone/UploadActivity.kt
+++ app/src/main/java/com/asimodabas/instagram_clone/UploadActivity.kt
@@ -2,10 +2,32 @@ package com.asimodabas.instagram_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asimodabas.instagram_clone.databinding.ActivityMainBinding
+import com.asimodabas.instagram_clone.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.uploadButton.setOnClickListener {
+            upload()
+        }
+        binding.imageView.setOnClickListener {
+            selectImage()
+        }
+    }
+
+    fun upload(){
+
+    }
+    fun selectImage(){
+
     }
 }
