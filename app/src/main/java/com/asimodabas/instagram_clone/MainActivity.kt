8a65d6dacfb6655b930ef6a966f45b89374b4588
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-12-29 11:40:14 +0300

    Create user and pass added

--- app/src/main/java/com/asimodabas/instagram_clone/MainActivity.kt
+++ app/src/main/java/com/asimodabas/instagram_clone/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.asimodabas.instagram_clone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.asimodabas.instagram_clone.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        auth = Firebase.auth
+
         binding.signInButton.setOnClickListener {
-           signInClicked()
+            signInClicked()
         }
 
         binding.signUpButton.setOnClickListener {
@@ -24,11 +31,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun signInClicked(){
+    fun signInClicked() {
 
     }
 
-    fun signUpClicked(){
+    fun signUpClicked() {
 
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordText.text.toString()
+
+        if (email.equals("") || password.equals("")) {
+            Toast.makeText(this, "Enter E-Mail and Password!", Toast.LENGTH_LONG).show()
+        } else {
+            auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+                //Success
+                val intent = Intent(this@MainActivity,FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                //Failed
+                Toast.makeText(this,it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
