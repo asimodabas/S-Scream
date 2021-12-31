a6c0ddd26d6b3840fc51486a22126bad0bc39270
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-12-31 19:41:59 +0300

    Menu added(SignOut and AddPost)

--- app/src/main/java/com/asimodabas/instagram_clone/FeedActivity.kt
+++ app/src/main/java/com/asimodabas/instagram_clone/FeedActivity.kt
@@ -1,11 +1,51 @@
 package com.asimodabas.instagram_clone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.asimodabas.instagram_clone.databinding.ActivityFeedBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFeedBinding
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feed)
+        binding = ActivityFeedBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = Firebase.auth
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.insta_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_post){
+            val intent = Intent(this,UploadActivity::class.java)
+            startActivity(intent)
+        }else if (item.itemId == R.id.signout){
+            auth.signOut()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
