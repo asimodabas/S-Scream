2e1d86e9546f8ec85824a5314000a1225bfc445f
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-01-01 20:12:54 +0300

    started permissions

--- app/src/main/java/com/asimodabas/instagram_clone/UploadActivity.kt
+++ app/src/main/java/com/asimodabas/instagram_clone/UploadActivity.kt
@@ -1,12 +1,24 @@
 package com.asimodabas.instagram_clone
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.asimodabas.instagram_clone.databinding.ActivityMainBinding
 import com.asimodabas.instagram_clone.databinding.ActivityUploadBinding
+import com.google.android.material.snackbar.Snackbar
 
 class UploadActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityUploadBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,17 +29,31 @@ class UploadActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.uploadButton.setOnClickListener {
-            upload()
+            upload(it)
         }
         binding.imageView.setOnClickListener {
-            selectImage()
+            selectImage(it)
         }
     }
 
-    fun upload(){
+    fun upload(view: View) {
 
     }
-    fun selectImage(){
 
+    fun selectImage(view: View) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view,"Permission needed",Snackbar.LENGTH_INDEFINITE).setAction("Give permission"){
+                    //Request permission
+                }.show()
+            }else{
+                //Request permission
+
+            }
+        }else{
+            val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //Activity result
+        }
     }
 }
