9b27403107b8df885bf3c5e3793cd1667a5a8212
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-01-03 10:28:20 +0300

    array and post addded

--- app/src/main/java/com/asimodabas/instagram_clone/FeedActivity.kt
+++ app/src/main/java/com/asimodabas/instagram_clone/FeedActivity.kt
@@ -5,15 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.asimodabas.instagram_clone.databinding.ActivityFeedBinding
+import com.asimodabas.instagram_clone.model.Post
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityFeedBinding
     private lateinit var auth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
+    private lateinit var postArrayList: ArrayList<Post>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,38 @@ class FeedActivity : AppCompatActivity() {
         setContentView(view)
 
         auth = Firebase.auth
+        db = Firebase.firestore
+
+        postArrayList = ArrayList<Post>()
+
+        getData()
+    }
+
+    private fun getData(){
+        db.collection("Posts").addSnapshotListener { value, error ->
+        if (error != null){
+            Toast.makeText(this,error.localizedMessage,Toast.LENGTH_LONG).show()
+        }else{
+            if (value != null){
+                if (!value.isEmpty){
+                    val documents = value.documents
+                    for (document in documents){
+
+                        val comment = document.get("comment") as String
+                        val userEmail = document.get("userEmail") as String
+                        val downloadUrl = document.get("downloadUrl") as String
+
+                        val post = Post(userEmail,comment,downloadUrl)
+
+                        postArrayList.add(post)
+
+                    }
+                }
+            }
+        }
+
+
+        }
 
     }
 
---
+++ app/src/main/java/com/asimodabas/instagram_clone/model/Post.kt
@@ -0,0 +1,5 @@
+package com.asimodabas.instagram_clone.model
+
+data class Post(val email: String, val comment: String, val downloadUrl: String) {
+
+}
