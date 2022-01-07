c8738b7349c8bb0ed004588aef17496b2c7714f0
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-01-07 18:43:41 +0300

    trackBoolean and location permission

--- app/src/main/java/com/asimodabas/instagram_clone/view/MapsActivity.kt
+++ app/src/main/java/com/asimodabas/instagram_clone/view/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.asimodabas.instagram_clone.view
 
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -28,7 +29,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapsBinding
     private lateinit var locationManager: LocationManager
     private lateinit var locationListener: LocationListener
-    private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var sharedPreferences: SharedPreferences
+    private var trackBoolean: Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +45,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         registerLauncher()
+        sharedPreferences = this.getSharedPreferences(
+            "com.asimodabas.instagram_clone.view",
+            MODE_PRIVATE)
+        trackBoolean = false
     }
 
     /**
@@ -60,37 +67,87 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         locationListener = object : LocationListener {
             override fun onLocationChanged(p0: Location) {
+                trackBoolean = sharedPreferences.getBoolean("trackBoolean",false)
+                if (!trackBoolean!!){
+                    val userLocation = LatLng(p0.latitude, p0.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+                    sharedPreferences.edit().putBoolean("trackBoolean",true).apply()
+                }
 
             }
         }
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.ACCESS_FINE_LOCATION)){
-                Snackbar.make(binding.root,"Permission needed !",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
-                    //request permission
-                    permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
-                }.show()
-            }else{
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                Snackbar.make(binding.root, "Permission needed !", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Give Permission") {
+                        //request permission
+                        permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    }.show()
+            } else {
                 //request permission
                 permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
             }
         } else {
             //permission granted
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0f,
+                locationListener
+            )
+            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (lastLocation != null) {
+                val lastUserLocation = LatLng(lastLocation.latitude, lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f))
+            }
+            mMap.isMyLocationEnabled = true
         }
 
     }
-    private fun registerLauncher(){
-        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){result ->
-            if (result){
-                    if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
-                        //permission granted
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
-                    }
-            }else{
-                //permission denied
-                Toast.makeText(this@MapsActivity,"Permission needed !",Toast.LENGTH_LONG).show()
-            }
 
-        }
+    private fun registerLauncher() {
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (result) {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        //permission granted
+                        locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            0,
+                            0f,
+                            locationListener
+                        )
+                        val lastLocation =
+                            locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                        if (lastLocation != null) {
+                            val lastUserLocation =
+                                LatLng(lastLocation.latitude, lastLocation.longitude)
+                            mMap.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    lastUserLocation,
+                                    15f
+                                )
+                            )
+                        }
+                    }
+                } else {
+                    //permission denied
+                    Toast.makeText(this@MapsActivity, "Permission needed !", Toast.LENGTH_LONG)
+                        .show()
+                }
+
+            }
     }
 }
